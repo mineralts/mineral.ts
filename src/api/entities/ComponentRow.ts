d020c0a5d78ef02e5e4e64d6b4339ce821001574
leadcodedev <baptiste.parmantier.pro@gmail.com>
2021-08-27 22:42:36 +0200

    :construction: Implement row proxy

--- src/api/entities/ComponentRow.ts
+++ src/api/entities/ComponentRow.ts
@@ -2,7 +2,16 @@ import { ComponentType } from '../../types'
 
 export default class ComponentRow {
   public type: ComponentType = ComponentType.ACTION_ROW
-  public components: any[] = []
+  public components: any[] = new Proxy([], {
+    set: function(target, property, value, receiver) {
+      if (target.length > 5) {
+        throw new Error(`A row can contain a maximum of 5 components, ${target.length} given.`)
+      }
+      target[property] = value;
+      // you have to return true to accept the changes
+      return true;
+    }
+  })
 
   public addComponent (component: any) {
     this.components.push(component)
