7ae424623e103d527f5fa8d78f2c1792304f8f2d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-14 20:50:16 +0200

    :construction: Work in progress

--- src/api/entities/Guild.ts
+++ src/api/entities/Guild.ts
@@ -1,33 +1,59 @@
 import { Snowflake } from '../../types'
-import Collection from '@discordjs/collection'
-import Channel from './Channel'
+import GuildHashes from './GuildHashes'
+import GuildStickerManager from './GuildStickerManager'
+import GuildPresenceManager from './GuildPresenceManager'
+import GuildThreadManager from './GuildThreadManager'
+import GuildEmojiManager from './GuildEmojiManager'
+import GuildRoleManager from './GuildRoleManager'
+import GuildMemberManager from './GuildMemberManager'
+import GuildChannelManager from './GuildChannelManager'
 import GuildMember from './GuildMember'
-import Emoji from './Emoji'
-import Role from './Role'
 
 export default class Guild {
   constructor (
     public id: Snowflake,
-    public icon: string | null,
     public name: string,
-    public description: string,
-    public banner: any,
-    public memberCount: number,
-    public owner: GuildMember,
-    public members: Collection<Snowflake, GuildMember>,
-    public channels: Collection<Snowflake, Channel>,
-    public emojis: Collection<Snowflake, Emoji>,
-    public role: Collection<Snowflake, Role>,
-    public verificationLevel: number,
+    public icon: string | null,
+    public banner: string | null,
+    public splash: string | null,
+    public description: string | null,
     public premiumTier: number,
     public premiumSubscriptionCount: number,
+    public systemChannelFlags: number,
     public explicitContentFilter: number,
-    public maxVideoChannelUsers: number,
-    public splash: any,
-    public stickers: any[],
-    public unavailable: boolean,
-    public voiceState: any[],
     public region: string,
-    public ruleChannel: Channel | undefined
-  ) {}
+    public isLazy: boolean,
+    public applicationId: string | null,
+    public isNSFW: boolean,
+    public memberCount: number,
+    public roles: GuildRoleManager,
+    public stageInstances: [],
+    public guildHashes: GuildHashes,
+    public afkChannelId: Snowflake,
+    public channels: GuildChannelManager,
+    public verificationLevel: number,
+    public hasPremiumProgressBarEnabled: boolean,
+    public features: string[],
+    public stickers: GuildStickerManager,
+    public members: GuildMemberManager,
+    public ruleChannelId: Snowflake,
+    public presences: GuildPresenceManager,
+    public guildScheduledEvents: any[],
+    public defaultMessageNotifications: number,
+    public MFALevel: number,
+    public threads: GuildThreadManager,
+    public maxMemberSize: number,
+    public emojis: GuildEmojiManager,
+    public defaultLang: string,
+    public ownerId: Snowflake,
+    public owner: GuildMember,
+    public maxVideoChannelUsers: number,
+    public registeredCommandCount: number,
+    public applicationCommandCount: number,
+    public afkTimeout: number,
+    public systemChannelId: Snowflake,
+    public vanityUrlCode: string | null,
+    public embeddedActivities: any[],
+  ) {
+  }
 }
