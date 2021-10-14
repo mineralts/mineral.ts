8311b6cd15c7b29335b8f76a42a2f902572a2298
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-14 20:48:40 +0200

    :construction: Work in progress

--- src/packets/BasePacket.ts
+++ src/packets/BasePacket.ts
@@ -1,6 +1,6 @@
-import Client from '../client/Client'
+import Client from '../clients/MineralClient'
 
 export default abstract class BasePacket {
-  public abstract packetType: string
+  public packetType!: string
   public abstract handle (client: Client, payload: any): Promise<void>
 }
