df4af345e8bb42ecab85dcd6adcc437c40b1d685
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-14 20:48:59 +0200

    :construction: Work in progress

--- src/api/entities/Channel.ts
+++ src/api/entities/Channel.ts
@@ -1,31 +1,9 @@
-import { NumericChannelInstance, Snowflake } from '../../types'
+import { Snowflake } from '../../types'
 
 export default class Channel {
   constructor (
     public id: Snowflake,
-    public type: NumericChannelInstance,
-    public name: string,
-    // public readonly createdAt: Date
-    // public readonly createdTimestamp: number
-  ) {}
-
-  public async delete (reason?: string): Promise<Channel> {
-    return this
-  }
-
-  public async fetch (force: boolean): Promise<Channel> {
-    return this
-  }
-
-  public isText (): boolean {
-    return false;
-  }
-
-  public isThread (): boolean {
-    return false;
-  }
-
-  public isVoice (): boolean {
-    return false;
+    public type: any
+  ) {
   }
 }
