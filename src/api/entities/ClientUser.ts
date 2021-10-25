9e20d4f29b2b6a022e58ec8ef32809773f1c8dba
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-25 16:28:22 +0200

    :construction: Work in progress

--- src/api/entities/ClientUser.ts
+++ src/api/entities/ClientUser.ts
@@ -1,13 +1,32 @@
 import User from './User'
 import Guild from './Guild'
+import Command from '../components/commands/Command'
+import Request from '../../sockets/Request'
+import Presence from './Presence'
+import Collection from '@discordjs/collection'
+import { Snowflake } from '../../types'
 
 export default class ClientUser {
   constructor (
     public readonly user: User,
     public readonly sessionId: string,
-    public readonly presences: any[],
+    public readonly presences: Presence[],
     public readonly guilds: Guild[],
-    public readonly application: { id: string, flags: number }
+    public readonly application: { id: string, flags: number },
+    public commands: Collection<Snowflake, Command> = new Collection()
   ) {
+  }
+
+  public async registerCommands (...commands: Command[]) {
+    const request = new Request(`/applications/${this.application.id}`)
+
+    return Promise.all(
+      commands.map(async (command: Command) => {
+        const payload = await request.post(command)
+        command.id = payload.id
+
+        this.commands.set(command.id!, command)
+      })
+    )
   }
 }
