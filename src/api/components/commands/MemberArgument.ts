6ac26c662fd106ee2a9f8a4ab15c1a47f531b44f
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-25 16:28:37 +0200

    :construction: Work in progress

---
+++ src/api/components/commands/MemberArgument.ts
@@ -0,0 +1,23 @@
+import { CommandArgumentType } from '../../../types'
+
+export default class MemberArgument {
+  public name!: string
+  public description!: string
+  public isRequired: boolean = false
+  public readonly type: keyof typeof CommandArgumentType = 'USER'
+
+  public setName (value: string) {
+    this.name = value.toLowerCase()
+    return this
+  }
+
+  public setDescription (value: string) {
+    this.description = value
+    return this
+  }
+
+  public setRequired (value: boolean) {
+    this.isRequired = value
+    return this
+  }
+}
