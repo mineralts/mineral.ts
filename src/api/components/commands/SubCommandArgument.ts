daae8893d8a0a1034b5867cb6c15f2e728c56ff4
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-11-07 23:37:11 +0100

    :construction: Work in progress

--- src/api/components/commands/SubCommandArgument.ts
+++ src/api/components/commands/SubCommandArgument.ts
@@ -1,11 +1,16 @@
 import { CommandArgumentType, CommandParamsResolvable } from '../../../types'
 import StringArgument from './StringArgument'
+import NumberArgument from './NumberArgument'
+import ChoiceArgument from './ChoiceArgument'
+import MemberArgument from './MemberArgument'
+import ChannelArgument from './ChannelArgument'
 
 export default class SubCommandArgument {
   public type: keyof typeof CommandArgumentType = 'SUB_COMMAND'
   public name!: string
   public description!: string
   public arguments: CommandParamsResolvable[] = []
+  public defaultPermission: boolean = true
 
   public setName (name: string) {
     this.name = name.toLowerCase()
@@ -21,6 +26,39 @@ export default class SubCommandArgument {
     const argument = new StringArgument()
     callback(argument)
     this.arguments.push(argument)
+    return this
+  }
+
+  public addNumberArgument (callback: (argument: NumberArgument) => void) {
+    const argument = new NumberArgument()
+    callback(argument)
+    this.arguments.push(argument)
+    return this
+  }
+
+  public addChoiceArgument (callback: (argument: ChoiceArgument) => void) {
+    const argument = new ChoiceArgument()
+    callback(argument)
+    this.arguments.push(argument)
+    return this
+  }
+
+  public addMemberArgument (callback: (argument: MemberArgument) => void) {
+    const argument = new MemberArgument()
+    callback(argument)
+    this.arguments.push(argument)
+    return this
+  }
+
+  public addChannelArgument (callback: (argument: ChannelArgument) => void) {
+    const argument = new ChannelArgument()
+    callback(argument)
+    this.arguments.push(argument)
+    return this
+  }
+
+  public setDefaultPermission (value: boolean) {
+    this.defaultPermission = value
     return this
   }
 }
